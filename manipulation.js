66da2c63a42cf7bc5ac4f12445ae31829ebba637
Sicheng Zhu <zsc@uw.edu>
2018-04-16 22:00:43 -0700

    CRUD

---
+++ manipulation.js
@@ -0,0 +1,87 @@
+var employee = [{employeeid : 1, name : "Nancy Davolio", degree : "Bachelor"},
+			    {employeeid : 2, name : "Andrew Fuller", degree : "Doctor"},
+			    {employeeid : 3, name : "Janet Leverling", degree : "Bachelor"},
+			    {employeeid : 4, name : "Margaret Peacock", degree : "Bachelor"},
+			    {employeeid : 5, name : "Steven Buchanan", degree : "Bachelor"}];
+
+function add(parameters) {
+	var newEmpID;
+	var newEmp;
+	
+	if (parameters.employeeid !== undefined) {
+		for (var i = 0; i < employee.length; i++) {
+		    if (employee[i].employeeid.toString() === parameters.employeeid) {					
+				return false;
+			}
+		}
+				
+		newEmp = {employeeid : Number(parameters.employeeid), name : parameters.name, degree : parameters.degree};
+	} else {			
+		newEmp = {employeeid : employee[employee.length - 1].employeeid + 1, name : parameters.name, degree : parameters.degree};
+	}
+		
+	for (var prop in newEmp) {
+        if (newEmp[prop] === undefined) {
+			newEmp[prop] = "";
+		}
+	}
+	
+	employee.push(newEmp);	
+	return true;
+}
+
+function getAll() {
+	return employee;
+}
+
+function get(key, value) {
+	var isValidKeyName = false;
+
+	for (var keyWord in employee[0]) {		
+		if (key === keyWord) {
+			isValidKeyName = true;
+		}
+	}
+
+	if (!isValidKeyName) {					    
+		return undefined;
+	}
+	
+	for (var i = 0; i < employee.length; i++) {
+		if (employee[i][key].toString() === value) {	
+			return employee[i];
+		}
+	}	
+	
+	return undefined;
+}
+
+function remove(key, value) {
+	var isValidKeyName = false;
+
+	for (var keyWord in employee[0]) {		
+		if (key === keyWord) {
+			isValidKeyName = true;
+		}
+	}
+
+	if (!isValidKeyName) {					    
+		return false;
+	}
+	
+	for (var i = 0; i < employee.length; i++) {
+		if (employee[i][key].toString() === value) {
+			employee.splice(i, 1);
+			return true;
+		}
+	}	
+	
+	return false;
+}
+
+module.exports = {
+    add: add,
+    getAll: getAll,
+    get: get,
+    remove: remove
+};
